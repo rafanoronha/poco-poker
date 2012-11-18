be58df78e45507e95cebb3febb2ba0b1c908421b
rafanoronha <rafanoronha@gmail.com>
2012-11-18 10:13:15 -0200

    first steps designing PocoPoker.Betting

---
+++ PocoPoker/PocoPoker.Betting/Action.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocoPoker.Betting
+{
+    public abstract class Action
+    {
+        public Player Player { get; private set; }
+    }
+
+    public class Check : Action
+    {
+    }
+
+    public class Bet : Action
+    {
+    }
+
+    public class Fold : Action
+    {
+    }
+
+    public class Call : Action
+    {
+    }
+
+    public class Raise : Action
+    {
+    }
+}
---
+++ PocoPoker/PocoPoker.Betting/BettingRound.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocoPoker.Betting
+{
+    public class BettingRound
+    {
+        public BettingRound(Positions positions)
+        {
+            Positions = positions;
+        }
+
+        public void PlaceAction(Action action)
+        {
+        }
+
+        public Turn CurrentTurn { get; private set; }
+        public Positions Positions { get; private set; }
+        public Pot Pot { get; private set; }
+    }
+
+    public class Positions
+    {
+        public Position Dealer { get; private set; }
+        public Position SmallBlind { get; private set; }
+        public Position BigBlind { get; private set; }
+        public Position UnderTheGun { get; private set; }
+        public Position Cutoff { get; private set; }
+        public IEnumerable<Position> MiddlePositions { get; private set; }
+    }
+
+    public class Position
+    {
+        public Player Player { get; private set; }
+        public Position NextPosition { get; private set; }
+    }
+
+}
